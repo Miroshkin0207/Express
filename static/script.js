a321e6daf3769aba16f94e173d57e0c7bf893007
Miroshkin <vovamiroshkin2011@gmail.com>
2026-09-11 21:46:14 +0300

    Пробуем сделать для Vercel

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.querySelector("button").onclick = async () =>
 {
     let text;
-    const answer = await fetch("http://localhost:3000/data", {
+    const answer = await fetch("https://expressmir.vercel.app/data", {
         method: "GET"
     });
     text = await answer.text();
@@ -11,7 +11,7 @@ document.querySelector("button").onclick = async () =>
 document.querySelectorAll("button")[1].onclick = async () =>
 {
     let text;
-    const answer = await fetch("http://localhost:3000/setData", {
+    const answer = await fetch("https://expressmir.vercel.app/setData", {
         headers: { "Content-Type": "application/json" },
         method: "POST",
         body: JSON.stringify({ n: 5 })
