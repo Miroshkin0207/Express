6817706a7398c62014f35edeb7c176cb5e383fa2
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2026-09-12 19:48:40 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.querySelector("button").onclick = async () =>
 {
     let text;
-    const answer = await fetch("https://expressmir.vercel.app/api/data", {
+    const answer = await fetch("https://expressmir.vercel.app/api/server/data", {
         method: "GET"
     });
     text = await answer.text();
@@ -11,7 +11,7 @@ document.querySelector("button").onclick = async () =>
 document.querySelectorAll("button")[1].onclick = async () =>
 {
     let text;
-    const answer = await fetch("https://expressmir.vercel.app/api/setData", {
+    const answer = await fetch("https://expressmir.vercel.app/api/server/setData", {
         headers: { "Content-Type": "application/json" },
         method: "POST",
         body: JSON.stringify({ n: 5 })
